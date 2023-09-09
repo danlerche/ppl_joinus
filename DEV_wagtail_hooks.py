a51b36b35031fad10ed3bf5b62f6b278223583c1
Dan Lerch <dan.lerch@gmail.com>
2023-09-09 11:51:41 -0700

    uploaded project files

---
+++ DEV_wagtail_hooks.py
@@ -0,0 +1,306 @@
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, modeladmin_register)
+from wagtail.contrib.modeladmin.views import IndexView
+from .models import JoinusEvent, JoinusUserFormBuilder, JoinusRegistration, JoinusFormPage
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import format_html
+from django.http import HttpResponse
+from wagtail.core import hooks
+import json, itertools, csv
+import csv
+from django.http import HttpResponse
+from django.utils.encoding import smart_str
+from django.contrib.auth.decorators import login_required
+from django.conf.urls import url
+from django.urls import reverse
+from django.utils.translation import ugettext as _
+from django.utils.decorators import method_decorator
+from django.core.exceptions import PermissionDenied
+from wagtail.contrib.modeladmin.views import IndexView
+from wagtail.contrib.modeladmin.helpers import AdminURLHelper, ButtonHelper
+
+
+class EventAdmin(ModelAdmin):
+    model = JoinusFormPage
+    menu_label = 'Registration Forms'
+    add_to_settings_menu = True
+    exclude_from_explorer = False
+
+
+class RegistrationFormsAdmin(ModelAdmin):
+    model = JoinusEvent
+    menu_label = 'Events'
+    add_to_settings_menu = True
+    exclude_from_explorer = False
+
+
+
+class ExportButtonHelper(ButtonHelper):
+
+    export_all_events_button_classnames = ['icon', 'icon-download']
+    export_single_event_button_classnames = ['icon', 'icon-download']
+
+    def export_all_events_button(self, classnames_add=None, classnames_exclude=None):
+        if classnames_add is None:
+            classnames_add = []
+        if classnames_exclude is None:
+            classnames_exclude = []
+
+        classnames = self.export_all_events_button_classnames + classnames_add
+        cn = self.finalise_classname(classnames, classnames_exclude)
+        text = _('Export {}'.format(self.verbose_name_plural.title()))
+
+        return {
+            'url': self.url_helper.get_action_url('export_all_events', query_params=self.request.GET),
+            'label': text,
+            'classname': cn,
+            'title': text,
+        }
+
+    def export_single_event_button(self, classnames_add=None, classnames_exclude=None):
+        if classnames_add is None:
+            classnames_add = []
+        if classnames_exclude is None:
+            classnames_exclude = []
+
+        classnames = self.export_single_event_button_classnames + classnames_add
+        cn = self.finalise_classname(classnames, classnames_exclude)
+        text = _('Export {}'.format(self.verbose_name_plural.title()))
+
+        return {
+            'url': self.url_helper.get_action_url('export_single_event', query_params=self.request.GET),
+            'label': text,
+            'classname': cn,
+            'title': text,
+        }
+
+class ExportAdminURLHelper(AdminURLHelper):
+    non_object_specific_actions = ('create', 'choose_parent', 'index', 'export_all_events', 'export_single_event')
+
+    def get_action_url(self, action, *args, **kwargs):
+        query_params = kwargs.pop('query_params', None)
+
+        url_name = self.get_action_url_name(action)
+        if action in self.non_object_specific_actions:
+            url = reverse(url_name)
+        else:
+            url = reverse(url_name, args=args, kwargs=kwargs)
+
+        if query_params:
+            url += '?{params}'.format(params=query_params.urlencode())
+
+        return url
+
+    def get_action_url_pattern(self, action):
+        if action in self.non_object_specific_actions:
+            return self._get_action_url_pattern(action)
+
+        return self._get_object_specific_action_url_pattern(action)
+
+
+class ExportAllEventsView(IndexView):
+
+    def export_all_events_csv(self):
+        data = self.queryset.all()
+
+        data_headings = [field.verbose_name for field
+                         in JoinusRegistration._meta.get_fields()]
+
+        del data_headings[1] #removes the user_info heading as it is replaced by parsed json keys
+
+        # return a CSV instead
+        response = HttpResponse(content_type='text/csv; charset=utf-8')
+        response['Content-Disposition'] = 'attachment;filename=' + \
+            'all_event_registrations.csv'
+
+        # Prevents UnicodeEncodeError for labels with non-ansi symbols
+        data_headings = [smart_str(label) for label in data_headings]
+
+        writer = csv.writer(response)
+
+        user_csv_headings_list = []
+
+        for heading_info in data:
+            user_json = str(heading_info.user_info)
+            json_loads = json.loads(user_json)
+            user_csv_headings_list = list(json_loads.keys())
+
+        first_three_headings =  user_csv_headings_list[0:3]
+        writer.writerow(data_headings + first_three_headings)
+
+        for reg in data:
+            if reg.wait_list == True:
+                reg.wait_list = 'Yes'
+            else:
+                reg.wait_list = 'No'
+            user_csv_values_list = []
+            user_json = str(reg.user_info)
+            json_loads = json.loads(user_json)
+            user_csv_values_list = list(json_loads.values())
+            first_three_values = (user_csv_values_list[0:3])
+            data_row = []
+            data_row.extend([
+                reg.id, reg.event_name, reg.registration_date, reg.wait_list
+            ])
+
+            writer.writerow(data_row + first_three_values)
+
+        return response
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        super().dispatch(request, *args, **kwargs)
+        return self.export_all_events_csv()
+
+class ExportSingleEventView(IndexView):
+
+    def export_single_event_csv(self):
+        data = self.queryset.all()
+
+        data_headings = [field.verbose_name for field
+                         in JoinusRegistration._meta.get_fields()]
+
+        del data_headings[1] #removes the user_info heading as it is replaced by parsed json keys
+
+        # return a CSV instead
+        response = HttpResponse(content_type='text/csv; charset=utf-8')
+        response['Content-Disposition'] = 'attachment;filename=' + \
+            'event_registrations.csv'
+
+        # Prevents UnicodeEncodeError for labels with non-ansi symbols
+        data_headings = [smart_str(label) for label in data_headings]
+
+        writer = csv.writer(response)
+
+        user_csv_headings_list = []
+
+        for heading_info in data:
+            user_json = str(heading_info.user_info)
+            json_loads = json.loads(user_json)
+            user_csv_headings_list = list(json_loads.keys())
+
+        writer.writerow(data_headings + user_csv_headings_list)
+
+        for reg in data:
+            if reg.wait_list == True:
+                reg.wait_list = 'Yes'
+            else:
+                reg.wait_list = 'No'
+            user_csv_values_list = []
+            user_json = str(reg.user_info)
+            json_loads = json.loads(user_json)
+            user_csv_values_list = list(json_loads.values())
+            data_row = []
+            data_row.extend([
+                reg.id, reg.event_name, reg.registration_date, reg.wait_list
+            ])
+
+            writer.writerow(data_row + user_csv_values_list)
+
+        return response
+
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        super().dispatch(request, *args, **kwargs)
+        return self.export_single_event_csv()
+
+class ExportModelAdminMixin(object):
+    """
+    A mixin to add to your model admin which hooks the different helpers, the view and register the new urls.
+    """
+
+    button_helper_class = ExportButtonHelper
+    url_helper_class = ExportAdminURLHelper
+
+    export_all_events_view_class = ExportAllEventsView
+    export_single_event_view_class = ExportSingleEventView
+
+    def get_admin_urls_for_registration(self):
+        urls = super().get_admin_urls_for_registration()
+        urls += (
+            url(
+                self.url_helper.get_action_url_pattern('export_all_events'),
+                self.export_all_events_view,
+                name=self.url_helper.get_action_url_name('export_all_events')
+            ),
+            url(
+                self.url_helper.get_action_url_pattern('export_single_event'),
+                self.export_single_event_view,
+                name=self.url_helper.get_action_url_name('export_single_event')
+            ),
+        )
+
+        return urls
+
+    def export_all_events_view(self, request):
+        kwargs = {'model_admin': self}
+        view_class = self.export_all_events_view_class
+        return view_class.as_view(**kwargs)(request)
+
+    def export_single_event_view(self, request):
+        kwargs = {'model_admin': self}
+        view_class = self.export_single_event_view_class
+        return view_class.as_view(**kwargs)(request)
+
+class RegistrationAdmin(ExportModelAdminMixin, ModelAdmin):
+    model = JoinusRegistration
+    menu_label = 'Event Registrations' 
+    list_display = ('event_name','user_info_parsed', 'registration_date', 'wait_list_rewrite', 'cancelled_rewrite',)
+
+    def user_info_parsed(self, obj):
+            user_info_str = str(obj.user_info)
+            user_info_loads = json.loads(user_info_str)
+            user_info_list = list(user_info_loads.values())
+            user_label_list = list(user_info_loads.keys())
+            format_label_info = ""
+            format_user_info = ""
+            pair_to_list = []
+            format_label_to_list = []
+            format_values_to_list = []
+            formated_html = ""
+
+            for add_th_tags in user_label_list:
+                        format_label_info = format_html('<span class="field-user_label_items col-sm"><strong>{}{}</strong></span>{}', add_th_tags, ': ', '*')
+                        format_label_to_list += format_label_info.split('*')
+                        del format_label_to_list[-1]
+
+            for add_td_tags in user_info_list:
+                        format_user_info = format_html('<span class="field-user_value_items col-sm">{}</span><br/>', add_td_tags)
+                        format_values_to_list += format_user_info.split('*')
+
+            for pair_lists_to_tuple in zip (format_label_to_list, format_values_to_list):
+                    pair_to_list = list(pair_lists_to_tuple)
+
+                    for i in pair_to_list:
+                        formated_html += format_html(i)
+
+            outer_html_beg = '<div class="field-reg-item">'
+            outer_html_end = '</div>'
+            outer_html = outer_html_beg + formated_html + outer_html_end
+            return format_html(outer_html)
+
+    def wait_list_rewrite(self, obj):
+                if obj.wait_list == 0:
+                    return 'No'
+                else: 
+                    return 'Yes'
+
+    def cancelled_rewrite(self, obj):
+                if obj.cancelled == 0:
+                    return 'No'
+                else: 
+                    return 'Yes'
+    
+    user_info_parsed.short_description = "Registraiton Info"
+    wait_list_rewrite.short_description = "Wait listed"
+    cancelled_rewrite.short_description = "Cancelled"
+    list_filter = ['event_name', 'wait_list', 'cancelled']
+    add_to_settings_menu = True
+    exclude_from_explorer = True
+    index_template_name = 'ppl_joinus/modeladmin/index.html'
+    button_helper_class = ExportButtonHelper
+
+modeladmin_register(EventAdmin)
+modeladmin_register(RegistrationAdmin)
+modeladmin_register(RegistrationFormsAdmin)
---
+++ models.py
@@ -0,0 +1,203 @@
+import json
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel, FieldRowPanel, PageChooserPanel
+from django import forms
+from wagtail.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractFormField, AbstractForm, AbstractEmailForm, AbstractFormSubmission
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
+from wagtail.models import Page
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+from django.conf import settings
+
+RESERVED_LABELS = ['Your Name', 'Email', 'Your Phone Number']
+
+def validate_label(value):
+    if value in RESERVED_LABELS:
+        raise ValidationError("'%s' is reserved." % value)
+
+class SuccessPage(Page):
+    body = RichTextField(blank=True)
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
+
+class JoinusEvent(Page):
+    body = RichTextField(blank=True)
+    date = models.DateTimeField()
+    spots_available = models.PositiveIntegerField(default=0)
+    waitlist_spots_available = models.PositiveIntegerField(default=0)
+    registration_form_chooser = models.ForeignKey('JoinusFormPage', default=1, blank=False, on_delete=models.SET_NULL, null=True)
+    notify_email = models.EmailField(max_length=254, blank="true", null=True, verbose_name="Notify Admin Email")
+    success_email_msg = RichTextField(blank=True, null=True, verbose_name="Body of the success email")
+    waitlist_email_msg = models.CharField(max_length=2000, blank=True, null=True, verbose_name="Body of the waitlist email")
+    success_page = models.ForeignKey('SuccessPage', default=1, blank=False, on_delete=models.SET_NULL, null=True)
+    content_panels = AbstractForm.content_panels + [
+        FieldPanel('body', classname="full"),
+        FieldPanel('date', classname="full"),
+        FieldPanel('spots_available', classname="full"),
+        FieldPanel('waitlist_spots_available', classname="full"),
+        FieldPanel('notify_email', classname="full"),
+        FieldPanel('success_email_msg', classname="full"),
+        FieldPanel('waitlist_email_msg', widget=forms.Textarea, classname="full"),
+        PageChooserPanel('registration_form_chooser'),
+        PageChooserPanel('success_page'),
+    ]
+
+    def serve(self, request, form_submission=None, *args, **kwargs):
+            event_instance = JoinusEvent.objects.get(id=self.page_ptr_id)
+            current_registered = JoinusRegistration.objects.filter(event_name_id=self.page_ptr_id, wait_list=0).count()
+            current_waitlisted = JoinusRegistration.objects.filter(event_name_id=self.page_ptr_id, wait_list=1).count()
+            cancelled_registered = JoinusRegistration.objects.filter(event_name_id=self.page_ptr_id, wait_list=0, cancelled=1).count()
+            cancelled_waitlist = JoinusRegistration.objects.filter(event_name_id=self.page_ptr_id, wait_list=1, cancelled=1).count()
+            current_spots = self.spots_available - current_registered + cancelled_registered
+            current_waitlist_spots = self.waitlist_spots_available - current_waitlisted  + cancelled_waitlist
+            
+
+            #Gets the registration page that relates to the form chooser selected by the user in the event page
+            #Gets the custom form that relates to the selected Registration form page
+            #custom_form = registration_form_page.get_form(request.POST, request.FILES, page=self, user=request.user)
+
+            if request.method == 'POST' and self.registration_form_chooser is not None:
+                registration_form_page = JoinusFormPage.objects.get(pk=self.registration_form_chooser)
+                custom_form = registration_form_page.get_form(request.POST, request.FILES, page=self, user=request.user)
+
+                if custom_form.is_valid() and current_spots > 0:
+                    form_submission = registration_form_page.process_form_submission(custom_form)
+                    user_instance = get_primary
+                    registration = JoinusRegistration(event_name=event_instance, user_info_id=get_primary[0], wait_list=0)
+                    registration.save()
+                    #There is a bug here. When the admin changes the spots available or the waitlist amount after people start registering
+                    messages.success(request, 'You have succesfully registered for ' + self.title)
+                    url = self.success_page.url
+                    
+                    #turning this off for now. Needs validation
+
+                    #submission_email = custom_form.cleaned_data['email']
+                    #subject = 'You have succesfully registered for ' + self.title
+                    #plain_message = strip_tags(self.success_email_msg)
+                    #from_email = settings.EMAIL_HOST_USER
+                    #recipient_list = [submission_email]
+                    #send_mail(subject, plain_message, from_email, [submission_email], html_message=self.success_email_msg)
+
+                    return redirect(url, permanent=False)
+
+                elif custom_form.is_valid() and current_spots == 0 and current_waitlist_spots > 0:
+                    form_submission = registration_form_page.process_form_submission(custom_form)
+                    user_instance = get_primary
+                    registration = JoinusRegistration(event_name=event_instance, user_info_id=get_primary[0], wait_list=1)
+                    registration.save()
+                    messages.success(request, 'You have been added to the ' + self.title + ' waitlist')
+                    url = self.success_page.url
+                    submission_email = custom_form.cleaned_data['email']
+                    send_mail(
+                        subject = 'You have been added to the waitlist for ' + self.title,
+                        message = self.success_email_msg,
+                        from_email = settings.EMAIL_HOST_USER,
+                        recipient_list = [submission_email],
+                        fail_silently=False,
+                    )
+
+                    return redirect(url, permanent=False)
+
+                else:
+                    return render(request, 'ppl_joinus/full.html', {
+                        'page': self,
+                    })
+
+            elif self.registration_form_chooser is not None:
+                registration_form_page = JoinusFormPage.objects.get(pk=self.registration_form_chooser)
+                user_form = registration_form_page.get_form(page=self, user=request.user)
+            else:
+                user_form = ''
+
+            return render(request, 'ppl_joinus/joinus_event.html', {
+                'page': self,
+                'current_registered': current_registered,
+                'current_waitlisted': current_waitlisted,
+                'current_spots': current_spots,
+                'cancelled_registered': cancelled_registered,
+                'cancelled_waitlist': cancelled_waitlist,
+                'current_waitlist_spots': current_waitlist_spots,
+                'event_instance': event_instance,
+                'custom_form': user_form,
+    })
+    class Meta:
+        verbose_name = "Joinus Event"
+
+class JoinusFormField(AbstractFormField):
+    form_builder_page = ParentalKey('JoinusFormPage', on_delete=models.CASCADE, related_name='form_fields')
+    label = models.CharField(
+        verbose_name='label',
+        max_length=255,
+        help_text='The label of the form field, cannot be one of the following: %s.'
+        % ', '.join(RESERVED_LABELS),
+        validators=[validate_label]
+    )
+
+class JoinusFormPage(AbstractEmailForm):
+
+    content_panels = AbstractForm.content_panels + [
+        FormSubmissionsPanel(),
+        InlinePanel('form_fields', label="Form fields"),
+    ]
+
+    def get_submission_class(self):
+        return JoinusUserFormBuilder
+
+    def process_form_submission(self, form):
+        create_submission = self.get_submission_class().objects.create(form_data=form.cleaned_data, page=self)
+        get_submission = self.get_submission_class().objects.filter(pk=create_submission.id).values('id')
+        global get_primary
+        get_primary = [id['id'] for id in get_submission]
+
+
+    def get_form_fields(self):
+
+            fields = list(super(JoinusFormPage, self).get_form_fields())
+
+            fields.insert(0, JoinusFormField(
+                label='Phone number',
+                field_type='singleline',
+                required=False,
+                help_text="Valid phone Numer"))
+
+            fields.insert(0, JoinusFormField(
+                label='Email',
+                field_type='email',
+                required=True,
+                help_text="Your email address"))
+
+            fields.insert(0, JoinusFormField(
+                label='Your Name',
+                field_type='singleline',
+                required=False,
+                help_text="Your First and Last Name"))
+
+            return fields
+
+    class Meta:
+        verbose_name = "Joinus Form Builder"
+
+
+class JoinusUserFormBuilder(AbstractFormSubmission):
+    pass
+
+class JoinusRegistration(models.Model):
+    user_info = models.ForeignKey('JoinusUserFormBuilder', default=1, on_delete=models.CASCADE)
+    event_name = models.ForeignKey('JoinusEvent', default=1, on_delete=models.CASCADE)
+    registration_date = models.DateTimeField(auto_now_add=True, blank=True)
+    wait_list = models.BooleanField(default=0)
+    cancelled = models.BooleanField(default=0)
+
+
+    def __str__(self):
+        return self.event_name.title
+
+    class Meta:
+        verbose_name = "Event Registration"
